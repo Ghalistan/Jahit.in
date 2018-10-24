d37bd052f61a7f566dd31744cfefca4a86f53a2f
Ghalistan <gghalistan@gmail.com>
2018-10-25 00:46:50 +0700

    (WIP) Page Konfirmasi_Pembayaran
    Page Konfirmasi Pembayaran & Base Beranda

--- app/src/main/java/protel/jahitin/Beranda.java
+++ app/src/main/java/protel/jahitin/Beranda.java
@@ -1,12 +1,17 @@
 package protel.jahitin;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class Beranda extends AppCompatActivity {
+public class Beranda extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(R.layout.activity_beranda);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
     }

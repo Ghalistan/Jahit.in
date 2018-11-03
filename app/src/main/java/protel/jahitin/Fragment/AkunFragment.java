8eea53f526f56bffcc19185b279700dbaaa55235
Ghalistan <gghalistan@gmail.com>
2018-11-03 15:34:12 +0700

    alert dialog akun

--- app/src/main/java/protel/jahitin/Fragment/AkunFragment.java
+++ app/src/main/java/protel/jahitin/Fragment/AkunFragment.java
@@ -1,19 +1,24 @@
 package protel.jahitin.Fragment;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import protel.jahitin.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AkunFragment extends Fragment {
-
+public class AkunFragment extends Fragment implements View.OnClickListener{
+    ImageView editData;
 
     public AkunFragment() {
         // Required empty public constructor
@@ -21,10 +26,45 @@ public class AkunFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_akun, container, false);
+        View view = inflater.inflate(R.layout.fragment_akun, container, false);
+        editData = view.findViewById(R.id.edit_datadiri);
+        editData.setOnClickListener(this);
+
+        return view;
     }
 
+    public void alertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.item_alert_dialog_akun, null));
+        builder.setMessage("Edit Data Diri");
+        builder.setPositiveButton("Simpan", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edit_datadiri:
+                alertDialog();
+        }
+    }
+
+    private static int dpToPx(int dp) {
+        return (int)(dp * Resources.getSystem().getDisplayMetrics().density);
+    }
 }
